5d203650b56b89afdd40d0514465d354e26c6e52
karunathammanna123 <karunaprasad.t89@gmail.com>
2021-11-26 20:12:27 +0530

    Third commit

--- src/main/java/com/abc/App.java
+++ src/main/java/com/abc/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hi welcome to the Poll SCM JOB!" );
+        System.out.println( "Webhook is configured and is working fine!" );
     }
 }
